87aa7e342eb2fd2ffe7f398bd880a5342b57f5bd
Deepak Borse <deepak.borse27@gmail.com>
2022-11-10 22:59:33 +0530

    Conflict file updated

--- src/pages/conflict1.java
+++ src/pages/conflict1.java
@@ -7,8 +7,8 @@ public class conflict1 {
 		System.out.println("Project Uploaded Branch A and Branch B");
 		System.out.println("Project Uploaded ---------------------A");
 		System.out.println("Project Uploaded .....................A");
-		System.out.println("---------------------------------------");
-		System.out.println("ON Branch A using by Hybrid driven branch Deepak B A");
+		System.out.println("--------------------------------------");
+		System.out.println("ON Branch A");
 	}
 
 }
