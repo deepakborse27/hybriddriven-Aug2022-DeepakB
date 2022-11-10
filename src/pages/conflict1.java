242fd3c0401160e1a36a3cfb3c49f295701de19f
Deepak Borse <deepak.borse27@gmail.com>
2022-11-10 23:24:25 +0530

    B conflict resolved

--- src/pages/conflict1.java
+++ src/pages/conflict1.java
@@ -7,8 +7,10 @@ public class conflict1 {
 		System.out.println("Project Uploaded Branch A and Branch B");
 		System.out.println("Project Uploaded ---------------------A");
 		System.out.println("Project Uploaded .....................A");
-		System.out.println("--------------------------------------");
 		System.out.println("ON Branch A");
+		System.out.println("----------------------------------------");
+		System.out.println("ON Branch B Line 12 changes new line added");
+
 	}
 
 }
