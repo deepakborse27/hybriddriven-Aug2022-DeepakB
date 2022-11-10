b8591e3770b1843ae1414dfa534c0bade2bf9144
Deepak Borse <deepak.borse27@gmail.com>
2022-11-10 22:46:45 +0530

    Conflict file added

---
+++ src/pages/conflict1.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class conflict1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Project Uploaded Branch A and Branch B");
+		System.out.println("Project Uploaded ---------------------A");
+		System.out.println("Project Uploaded .....................A");
+		System.out.println("---------------------------------------");
+		System.out.println("ON Branch A using by Hybrid driven branch Deepak B A");
+	}
+
+}
